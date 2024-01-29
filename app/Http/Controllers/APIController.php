f18e3ab04dc549036b1031788b767e1b728d31bb
br3-ma <procoding94@gmail.com>
2024-01-29 18:38:03 +0200

    Route SMS

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 
 class APIController extends Controller
 {
-
     public function route_sms_send(Request $request){
+        $this->send_with_guzzle($request);
+    }
+
+    public function send_with_curl($request){
         $message = $request->input('message');
         $username = 'gtzm-mightyfin';
         $password = 'Mighty@2';
         
-        $type = '5';
+        $type = '0';
         $dlr = '1';
         $destination = $request->input('phone');
         $source = 'Mighty Finance Solution';
@@ -55,5 +60,43 @@ class APIController extends Controller
     
         return response()->json($responseData);
     }
+
+    public function send_with_guzzle($request){
+        $message = $request->input('message');
+        $username = 'gtzm-mightyfin';
+        $password = 'Mighty@2';
+        $type = '0';
+        $dlr = '1';
+        $destination = $request->input('phone');
+        $source = 'Mighty Finance Solution';
+        $url = "https://api.rmlconnect.net/bulksms/bulksms";
+        $client = new Client();
     
+        try {
+            // Make the HTTP request using Guzzle
+            $response = $client->get($url, [
+                'query' => [
+                    'username' => $username,
+                    'password' => $password,
+                    'type' => $type,
+                    'dlr' => $dlr,
+                    'destination' => $destination,
+                    'source' => $source,
+                    'message' => $message,
+                ],
+                'timeout' => 30, // Timeout in seconds
+                'connect_timeout' => 20, // Connection timeout in seconds
+                // 'verify' => false, // Disable SSL verification (not recommended in production)
+            ]);
+            // Handle the response, you might want to check for success or log the result
+            $responseData = json_decode($response->getBody(), true);
+            dd($responseData);
+            // ... (handle the $responseData as needed)
+            return response()->json($responseData);
+        } catch (RequestException $e) {
+            dd($e);
+            // Handle Guzzle request exception
+            echo 'Guzzle error: ' . $e->getMessage();
+        }
+    }
 }
