69d5a8fc1a7adc5ff9b1b5fe19288ca110e1569a
br3-ma <procoding94@gmail.com>
2024-01-30 19:40:38 +0200

    API config

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -97,17 +97,19 @@ class APIController extends Controller
         }
     }
 
-    public function send_with_server($request){
+    public function send_with_server($request) {
         $message = $request->input('message');
         $username = 'gtzm-mightyfin';
         $password = 'Mighty@2';
+    
         $type = '0';
         $dlr = '1';
         $destination = $request->input('phone');
         $source = 'Mighty Finance Solution';
+    
         // API endpoint
         $apiEndpoint = "http://rslr.connectbind.com:8080/bulksms/bulksms";
-
+    
         // Build the query parameters
         $queryParams = http_build_query([
             'username' => $username,
@@ -118,32 +120,33 @@ class APIController extends Controller
             'source' => $source,
             'message' => $message,
         ]);
-
+    
         // Full API URL with query parameters
         $apiUrl = "{$apiEndpoint}?{$queryParams}";
-
+    
         // Initialize cURL session
         $ch = curl_init();
-
-        // Set cURL options
+    
+        // Set cURL options for GET request
         curl_setopt($ch, CURLOPT_URL, $apiUrl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
+    
         // Execute cURL session and get the response
         $response = curl_exec($ch);
-
+    
         // Check for cURL errors
         if (curl_errno($ch)) {
             // Handle cURL error
             echo 'Curl error: ' . curl_error($ch);
         }
-
+    
         // Close cURL session
         curl_close($ch);
-
+    
         // Return the API response
         return $response;
     }
+    
 
 
     // public function bulkSMS(){
