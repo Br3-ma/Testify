e2e28323a33f6622ebac4f4dd597f0728b9ba5bb
br3-ma <procoding94@gmail.com>
2024-01-30 19:27:10 +0200

    API config

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -98,52 +98,51 @@ class APIController extends Controller
     }
 
     public function send_with_server($request){
-    $message = $request->input('message');
-    $username = 'gtzm-mightyfin';
-    $password = 'Mighty@2';
-    
-    $type = '0';
-    $dlr = '1';
-    $destination = $request->input('phone');
-    $source = 'Mighty Finance Solution';
-            // API endpoint
-    $apiEndpoint = "http://rslr.connectbind.com:8080/bulksms/bulksms";
+        $message = $request->input('message');
+        $username = 'gtzm-mightyfin';
+        $password = 'Mighty@2';
+        $type = '0';
+        $dlr = '1';
+        $destination = $request->input('phone');
+        $source = 'Mighty Finance Solution';
+        // API endpoint
+        $apiEndpoint = "http://rslr.connectbind.com:8080/bulksms/bulksms";
 
-    // Build the query parameters
-    $queryParams = http_build_query([
-        'username' => $username,
-        'password' => $password,
-        'type' => $type,
-        'dlr' => $dlr,
-        'destination' => $destination,
-        'source' => $source,
-        'message' => $message,
-    ]);
+        // Build the query parameters
+        $queryParams = http_build_query([
+            'username' => $username,
+            'password' => $password,
+            'type' => $type,
+            'dlr' => $dlr,
+            'destination' => $destination,
+            'source' => $source,
+            'message' => $message,
+        ]);
 
-    // Full API URL with query parameters
-    $apiUrl = "{$apiEndpoint}?{$queryParams}";
+        // Full API URL with query parameters
+        $apiUrl = "{$apiEndpoint}?{$queryParams}";
 
-    // Initialize cURL session
-    $ch = curl_init();
+        // Initialize cURL session
+        $ch = curl_init();
 
-    // Set cURL options
-    curl_setopt($ch, CURLOPT_URL, $apiUrl);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // Set cURL options
+        curl_setopt($ch, CURLOPT_URL, $apiUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    // Execute cURL session and get the response
-    $response = curl_exec($ch);
+        // Execute cURL session and get the response
+        $response = curl_exec($ch);
 
-    // Check for cURL errors
-    if (curl_errno($ch)) {
-        // Handle cURL error
-        echo 'Curl error: ' . curl_error($ch);
-    }
+        // Check for cURL errors
+        if (curl_errno($ch)) {
+            // Handle cURL error
+            echo 'Curl error: ' . curl_error($ch);
+        }
 
-    // Close cURL session
-    curl_close($ch);
+        // Close cURL session
+        curl_close($ch);
 
-    // Return the API response
-    return $response;
+        // Return the API response
+        return $response;
     }
 
 
